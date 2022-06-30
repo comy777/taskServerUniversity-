bb5523e1773653468357a33c56177d89e91980a7
comy777 <carrillosebastian1@gmail.com>
2022-06-30 14:36:04 -0500

    Get tasks router fixed

--- src/router/task.ts
+++ src/router/task.ts
@@ -1,45 +1,23 @@
-import { Router } from "express";
-import { validateToken } from "../jwt/jwt";
-import { check } from "express-validator";
-import { validate } from "../middlewares/validate";
-import {
-  getTasks,
-  saveTask,
-  editTask,
-  deleteTask,
-  completeTask,
-} from "../controllers/task";
+import { Router } from 'express';
+import { validateToken } from '../jwt/jwt';
+import { check } from 'express-validator';
+import { validate } from '../middlewares/validate';
+import { getTasks, saveTask, editTask, deleteTask, completeTask } from '../controllers/task';
 
 const taskRouter = Router();
 
-taskRouter.get(
-  "/:id",
-  [validateToken, check("id", "Id no valido").isMongoId(), validate],
-  getTasks
-);
+taskRouter.get('/:lesson', [ validateToken, check('id', 'Id no valido').isMongoId(), validate ], getTasks);
 
-taskRouter.post(
-  "/:id",
-  [validateToken, check("id", "No es un id valido").isMongoId(), validate],
-  saveTask
-);
+taskRouter.post('/:id', [ validateToken, check('id', 'No es un id valido').isMongoId(), validate ], saveTask);
+
+taskRouter.put('/:id', [ validateToken, check('id', 'No es un id valido').isMongoId(), validate ], editTask);
+
+taskRouter.delete('/:id', [ validateToken, check('id', 'No es un id valido').isMongoId(), validate ], deleteTask);
 
 taskRouter.put(
-  "/:id",
-  [validateToken, check("id", "No es un id valido").isMongoId(), validate],
-  editTask
-);
-
-taskRouter.delete(
-  "/:id",
-  [validateToken, check("id", "No es un id valido").isMongoId(), validate],
-  deleteTask
-);
-
-taskRouter.put(
-  "/complete/:id",
-  [validateToken, check("id", "No es un id valido").isMongoId(), validate],
-  completeTask
+	'/complete/:id',
+	[ validateToken, check('id', 'No es un id valido').isMongoId(), validate ],
+	completeTask
 );
 
 export default taskRouter;
